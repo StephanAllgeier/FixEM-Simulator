b9dbb4edd0dd90f068033de81aba147285af41e6
Fabian Anzlinger <uvuik@student.kit.edu>
2023-05-28 16:16:35 +0200

    Linethickness adjusted, Dictionaries for datasets implemented

--- ProcessingDataPipeline/visualize_xyt.py
+++ ProcessingDataPipeline/visualize_xyt.py
@@ -1,17 +1,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-def plot_xy(x, y, t):
-    plt.plot(t, x, label='x')
-    plt.plot(t, y, label='y')
+plt.rcParams['lines.linewidth'] = 1
+def plot_xy(dataset):
+    f = dataset['f']
+    t = dataset['time']
+    x = dataset['x_vals']
+    y = dataset['y_vals']
+    plt.plot(t/f, x, label='x')
+    plt.plot(t/f, y, label='y')
     plt.xlabel('Time')
     plt.ylabel('Position in arcmin')
     plt.title('Position over Time')
     plt.legend()
     plt.show()
 
-def plot_xy_trace(x,y):
+def plot_xy_trace(dataset):
+    x = dataset['x_vals']
+    y = dataset['y_vals']
     plt.plot(x, y)
     plt.xlabel('X in arcminutes')
     plt.ylabel('Y in arcminutes')
@@ -23,11 +29,12 @@ def plot_xy_trace(x,y):
 
 
 if __name__ == '__main__':
-    file = r"C:\Users\fanzl\bwSyncShare\Documents\Dataset\External\EyeMotionTraces_Roorda Vision Berkeley\10003L_001.csv"
+
+    file = r"E:\GazeBase\GazeBase_v2_0\Fixation_Only\S_1001_S1_FXS.csv"
     data = pd.read_csv(file)
-    time = data['TimeAxis']
-    x_vals = data['xx']
-    y_vals = data['yy']
-    plot_xy(x_vals, y_vals, time)
-    plot_xy_trace(x_vals, y_vals)
+    gazebase = {"f": 1000, "x_vals": data['x'], "y_vals": data['y'], "time": data['n']}
+    #roorda = {"f": 1920, "x_vals": data['xx'], "y_vals": data['yy'], "time": data['TimeStamp']}
+
+    plot_xy(gazebase)
+    plot_xy_trace(gazebase)
     print('Done')
