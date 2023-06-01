e6e49c8fc968705c533b0b32a1c02ecf72c182e1
Fabian Anzlinger <uvuik@student.kit.edu>
2023-06-01 17:33:54 +0200

    Added Lowpass Filter, added print statement for filter

--- ProcessingData/Preprocessing/Filtering.py
+++ ProcessingData/Preprocessing/Filtering.py
@@ -1,6 +1,7 @@
 import scipy
-from scipy.signal import butter, lfilter
+from scipy.signal import butter, lfilter, freqz, cheby1, cheby2, filtfilt
 import numpy as np
+import matplotlib.pyplot as plt
 import statsmodels.datasets
 
 
@@ -12,12 +13,21 @@ class Filtering():
         return fft, fftfreq
 
     @staticmethod
-    def butter_bandpass(lowcut, highcut, fs, order=5):
+    def butter_bandpass(lowcut, highcut, fs, order=5, print_filter=False):
         nyq = 0.5 * fs
         low = lowcut / nyq
         high = highcut / nyq
         b, a = butter(order, [low, high], btype='band')
+        if print_filter:
+            w, h = freqz(b, a)
+            plt.plot((fs * 0.5 / np.pi) * w, abs(h), label=f'Order={order}')
+            plt.title(f'Band-pass filtered signal, lowcut={lowcut}, highcut={highcut}')
+            plt.xlabel('Frequency (Hz)')
+            plt.ylabel('Gain')
+            plt.grid(True)
+            plt.legend(loc='best')
         return b, a
+
 
     @staticmethod
     def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
@@ -25,6 +35,24 @@ class Filtering():
         y = lfilter(b, a, data)
         return y
 
+    @staticmethod
+    def butter_bandpass_filter_zerophase(data, lowcut, highcut, fs, order=5):
+        b, a = Filtering.butter_bandpass(lowcut, highcut, fs, order=order)
+        y = signal.filtfilt(b, a, data, padlen=3 * max(len(a), len(b)))
+        return y
+A
+    @staticmethod
+    def butter_lowpass(highcut, fs, order=5):
+        nyq = 0.5 * fs
+        normal_cutoff = highcut / nyq
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        return b, a
+
+    @staticmethod
+    def butter_lowpass_filter(data, highcut, fs, order=5):
+        b, a = Filtering.butter_lowpass(highcut, fs, order=order)
+        y = lfilter(b, a, data)
+        return y
     @staticmethod
     def kalman_filter():
         return None
