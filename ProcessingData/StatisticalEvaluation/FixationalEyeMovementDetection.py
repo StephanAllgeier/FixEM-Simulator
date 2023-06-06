1d230659a51f2b9aa39917a4857c0b553b7d2fe1
Fabian Anzlinger <uvuik@student.kit.edu>
2023-06-06 18:01:10 +0200

    Nothing interesting changed

--- ProcessingData/StatisticalEvaluation/FixationalEyeMovementDetection.py
+++ ProcessingData/StatisticalEvaluation/FixationalEyeMovementDetection.py
@@ -8,18 +8,6 @@ from ProcessingData.Preprocessing.Filtering import Filtering
 
 
 class EventDetection():
-    @staticmethod
-    def find_micsacc(df, constant_dict, mindur=3, vfac=5):
-        '''
-        parameters:
-        df=dataframe to work with
-        constant_dict = dictionary belongig to the dataset with information about the structure of the dataset
-        coordinate = which coordinate to evaluate
-        '''
-        input_array = df[[constant_dict['x_col'], constant_dict['y_col']]].to_numpy()
-        micsacc = microsac_detection.microsacc(input_array, sampling=constant_dict['f'], mindur=mindur, vfac=vfac)
-        return micsacc
-
     # -------------------------------------------------------------------------------------------------------------------
     # Drift and Tremor, thus filtered signal necessary
     # Tremor: f =[50Hz, 100Hz]
@@ -46,3 +34,6 @@ class EventDetection():
                                                                                           highcut=highcut,
                                                                                           fs=constant_dict['f'], order=order))
         return return_frame
+
+
+
