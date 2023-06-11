8239d7edef09baa2a70b71aa87cf129190499b9c
Fabian Anzlinger <uvuik@student.kit.edu>
2023-06-11 17:13:24 +0200

    Changing name to color instead of colors to make unified.

--- ProcessingData/Visualize.py
+++ ProcessingData/Visualize.py
@@ -6,13 +6,13 @@ class Visualize():
     plt.rcParams['lines.linewidth'] = 1
 
     @staticmethod
-    def plot_xy(dataset, const_dict, colors=['blue', 'orange'], labels=['x', 'y']):
+    def plot_xy(dataset, const_dict, color=['blue', 'orange'], labels=['x', 'y']):
         f = const_dict['f']
         t = dataset[const_dict['time_col']] * const_dict['TimeScaling']
         x = dataset[const_dict['x_col']] * const_dict['ValScaling']
         y = dataset[const_dict['y_col']] * const_dict['ValScaling']
-        plt.plot(t, x, label=labels[0], color=colors[0])
-        plt.plot(t, y, label=labels[1], color=colors[1])
+        plt.plot(t, x, label=labels[0], color=color[0])
+        plt.plot(t, y, label=labels[1], color=color[1])
         plt.xlabel('Time')
         plt.ylabel('Position in arcmin')
         plt.title('Position over Time')
@@ -41,7 +41,7 @@ class Visualize():
         plt.show()
 
     @staticmethod
-    def print_microsacc(df, const_dict, micsac):
+    def print_microsacc(df, const_dict, micsac, color=['red', 'blue']):
         Visualize.plot_xy(df, const_dict)
         if isinstance(micsac, tuple):
             micsac_list = micsac[0]  # Either tuple or list
@@ -50,6 +50,6 @@ class Visualize():
         else:
             print('Microsaccade Input is neither a tupel nor a list')
         for microsaccade in micsac_list:
-            plt.axvline(microsaccade[0] / const_dict['f'], color='blue')  # plotting onset of microsaccade
-            plt.axvline(microsaccade[1] / const_dict['f'], color='red')  # plotting offset of microsaccade
+            plt.axvline(microsaccade[0] / const_dict['f'], color=color[0])  # plotting onset of microsaccade
+            plt.axvline(microsaccade[1] / const_dict['f'], color=color[1])  # plotting offset of microsaccade
 
