beaff4a2ba9d9e75eccce6da0086b0a2c8248e0d
Fabian Anzlinger <uvuik@student.kit.edu>
2023-06-11 17:11:31 +0200

    Added filtering within the find_micsac function. Renamed find_micsac function.

--- ProcessingData/StatisticalEvaluation/Microsaccades.py
+++ ProcessingData/StatisticalEvaluation/Microsaccades.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from ProcessingData.ExternalCode.EngbertMicrosaccadeToolboxmaster.EngbertMicrosaccadeToolbox import microsac_detection
+from ProcessingData.StatisticalEvaluation.FixationalEyeMovementDetection import EventDetection
 
 
 class Microsaccades():
@@ -16,7 +17,7 @@ class Microsaccades():
         return count
 
     @staticmethod
-    def find_micsacc(df, constant_dict, mindur=3, vfac=5):
+    def find_micsac(df, constant_dict, mindur=3, vfac=5):
         '''
         parameters:
         df=dataframe to work with, units of the traces is in degrees of visual angle
@@ -24,9 +25,11 @@ class Microsaccades():
         coordinate = which coordinate to evaluate
         returns tuple with tuple[0] = microsaccades
         '''
+        #Filtering Signal like in Paper "Eye Movement Analysis in Simple Visual Tasks"
+        df = EventDetection.filter_drift(df, constant_dict=constant_dict, highcut=40, order=5)
         input_array = df[[constant_dict['x_col'], constant_dict['y_col']]].to_numpy()
-        micsacc = microsac_detection.microsacc(input_array, sampling=constant_dict['f'], mindur=mindur, vfac=vfac)
-        return micsacc
+        micsac = microsac_detection.microsacc(input_array, sampling=constant_dict['f'], mindur=mindur, vfac=vfac)
+        return micsac
 
     @staticmethod
     def get_roorda_micsac(df):
