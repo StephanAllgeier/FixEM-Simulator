395e74d83c948526a5bfde9406287f782b7a897f
Fabian Anzlinger <uvuik@student.kit.edu>
2023-06-13 16:48:20 +0200

    Funktion "drift_only" hinzugefügt. Sie entfernt MIkrosakkaden und FIltert das SIgnal. Aktuell gibt es hier noch ein paar Probleme mit den Frequenzen die angegeben werden.

--- ProcessingData/StatisticalEvaluation/FixationalEyeMovementDetection.py
+++ ProcessingData/StatisticalEvaluation/FixationalEyeMovementDetection.py
@@ -5,6 +5,9 @@ import scipy
 import pandas as pd
 from ProcessingData.ExternalCode.EngbertMicrosaccadeToolboxmaster.EngbertMicrosaccadeToolbox import microsac_detection
 from ProcessingData.Preprocessing.Filtering import Filtering
+from ProcessingData.Preprocessing.Interpolation import Interpolation
+from ProcessingData.StatisticalEvaluation.Microsaccades import Microsaccades
+from ProcessingData.Visualize import Visualize
 
 
 class EventDetection():
@@ -26,14 +29,23 @@ class EventDetection():
 
     @staticmethod
     def filter_drift(df, constant_dict, highcut=40, order=5): #laut "Eye Movement Analysis in Simple Visual Tasks"
-        return_frame = copy.deepcopy(df)
-        return_frame[constant_dict['x_col']] = pd.Series(Filtering.butter_lowpass_filter(df[constant_dict['x_col']],
+        df[constant_dict['x_col']] = pd.Series(Filtering.butter_lowpass_filter(df[constant_dict['x_col']],
                                                                                 highcut=highcut,
                                                                                 fs=constant_dict['f'], order=order))
-        return_frame[constant_dict['y_col']] = pd.Series(Filtering.butter_lowpass_filter(df[constant_dict['y_col']],
+        df[constant_dict['y_col']] = pd.Series(Filtering.butter_lowpass_filter(df[constant_dict['y_col']],
                                                                                           highcut=highcut,
                                                                                           fs=constant_dict['f'], order=order))
-        return return_frame
+        return df
+
+    @staticmethod
+    def drift_only(df, const_dict, micsac_mindur=25, micsac_vfac=21):
+        #remove blinks
+        blink_rm = Interpolation.remove_blink_annot(df, const_dict)
+        #remove micsacs
+        removed_micsac = Microsaccades.remove_micsac(blink_rm, const_dict, mindur=micsac_mindur, vfac=micsac_vfac)
+        #Lowpassfiltering Signal
+        filtered = EventDetection.filter_drift(removed_micsac, const_dict)
+        return filtered, removed_micsac, blink_rm
 
 
 
