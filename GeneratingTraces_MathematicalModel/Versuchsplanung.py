7bdc3bd235c28a5e15a39a93f58cf0a455b9c621
Fabian Anzlinger <uvuik@student.kit.edu>
2023-10-06 16:38:14 +0200

    Veraltete Version, eigentlich nur noch mit Versuchsplanung_HPC.py gearbeitet

--- GeneratingTraces_MathematicalModel/Versuchsplanung.py
+++ GeneratingTraces_MathematicalModel/Versuchsplanung.py
@@ -70,27 +70,29 @@ def Versuchsplanung():
               index=False)
     print(len(all_combinations))
 
-def save_combination_evaluation_to_json():
-    float_options = [(20, 10, 0.1, 1.9), (50, 25, 0.002, 1.9), (50, 10, 0.001, 3.4), (50, 10, 0.002, 3.4),
-                     (50, 10, 0.01, 3.4), (50, 10, 0.1, 3.4), (50, 10, 0.001, 3.9), (50, 10, 0.005, 3.9),
-                     (50, 10, 0.01, 3.9), (100, 10, 0.001, 5.4), (100, 10, 0.002, 5.4), (100, 10, 0.01, 5.4),
-                     (100, 10, 0.05, 5.4), (100, 10, 0.001, 5.9), (100, 10, 0.005, 5.9), (100, 10, 0.05, 6.9)]
+def save_combination_evaluation_to_json(combinations=None, folder_to_save_to=None):
+    if combinations is None:
+        print('Bitte Kombinationsmöglichkeiten eingeben.')
+    else:
+        combinations_to_eval = []
+        for comb in combinations:
+            combinations_to_eval.append((comb['simulation rate'], comb['cells per degree'], comb['relaxation rate'], comb['h_crit']))
     drop_var = ['simulation_freq', "potential_resolution", "relaxation_rate", "hc"]
-    n = 100
+    n = 50
     headers = ["simulation rate", "cells per degree", "relaxation rate", 'h_crit', "mean intermicsac duration [s]",
                "median intermicsac duration [s]", "stdev intermicsac duration [s]", "mean micsac amp [deg]",
                "median micsac amp [deg]", "stdev micsac amp [deg]", "Number of Micsac - Mean",
                "Number of Micsac - Median"]
     all_data = []
-    for combination in float_options:
+    for comb in combinations_to_eval:
         comb_micsac_amp = []
         comb_intermic_dur = []
         comb_micsacs = []
         for i in range(n):
-            simulation_rate = combination[0]
-            cells_per_deg = combination[1]
-            relaxation_rate = combination[2]
-            hc = combination[3]
+            simulation_rate = comb[0]
+            cells_per_deg = comb[1]
+            relaxation_rate = comb[2]
+            hc = comb[3]
             micsac_amp, intermic_duration, num_of_micsac = RandomWalkBased.RandomWalk.randomWalk(
                 simulation_freq=simulation_rate, potential_resolution=cells_per_deg, relaxation_rate=relaxation_rate,
                 hc=hc, duration=30, sampling_frequency=500, number_id=i)
@@ -99,7 +101,7 @@ def save_combination_evaluation_to_json():
             comb_micsacs.extend([num_of_micsac])
         my_dict = {"MicsacAmplitudes": comb_micsac_amp, "IntermicDur": comb_intermic_dur, "NumMicsac": comb_micsacs}
         with open(
-                fr'C:\Users\uvuik\bwSyncShare\Documents\Versuchsplanung Mathematisches Modell\30s\AmpDurNum_simrate={str(simulation_rate)},Cells={str(cells_per_deg)},relaxationrate={str(relaxation_rate)},hc={str(hc)}.json',
+                fr'{folder_to_save_to}\AmpDurNum_simrate={str(simulation_rate)},Cells={str(cells_per_deg)},relaxationrate={str(relaxation_rate)},hc={str(hc)}_n={n}.json',
                 'w') as fp:
             json.dump(my_dict, fp, indent=4)
 def get_combination_from_excel(excel_file):
@@ -112,12 +114,8 @@ def get_combination_from_excel(excel_file):
         all_combinations.append(tupel1)
     wb.close()
     return all_combinations
-
-if __name__ == '__main__':
-    excel_file=r"C:\Users\uvuik\bwSyncShare\Documents\Versuchsplanung Mathematisches Modell\30s, nur zwischen 10-30s Auswerten. Komibnationsmöglichkeiten.xlsx"
-    #Open Excelfile
-    combinations = get_combination_from_excel(excel_file)
-    j=0
+def alt():
+    j = 0
     headers = ["simulation rate", "cells per degree", "relaxation rate", 'h_crit', "mean intermicsac duration [s]",
                "median intermicsac duration [s]", "stdev intermicsac duration [s]", "mean micsac amp [deg]",
                "median micsac amp [deg]", "stdev micsac amp [deg]", "Number of Micsac - Mean",
@@ -130,7 +128,7 @@ if __name__ == '__main__':
         comb_micsac_amp = []
         comb_intermic_dur = []
         comb_micsacs = []
-        n=50
+        n = 50
         for i in range(n):
             simulation_rate = combination['simulation rate']
             cells_per_deg = combination['cells per degree']
@@ -169,5 +167,9 @@ if __name__ == '__main__':
               index=False)
 
 
-
-
+if __name__ == '__main__':
+    excel_file=r"C:\Users\fanzl\bwSyncShare\Documents\Versuchsplanung Mathematisches Modell\NeueKombinationen\MicsacStatistics30s25_filtered.xlsx"
+    #Open Excelfile
+    combinations = get_combination_from_excel(excel_file)
+    folder_to_save_to = r"C:\Users\fanzl\bwSyncShare\Documents\Versuchsplanung Mathematisches Modell\NeueKombinationen"
+    save_combination_evaluation_to_json(combinations=combinations, folder_to_save_to=folder_to_save_to)
