a3b7d15491f3340da2d4ffc1260c6671303b5c5e
uvuik <uvuik@student.kit.edu>
2023-10-27 16:43:50 +0200

    Init-Files erstellt

---
+++ GeneratingTraces_RGANtorch/FEM/__init__.py
@@ -0,0 +1,10 @@
+import logging
+import os
+import numpy as np
+import torch
+from pathlib import Path
+def make_logger(file_: str = "NO_FILE") -> logging.Logger:
+    log_level = getattr(logging, os.getenv("LOG_LEVEL", "INFO"))
+    fmt = "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+    logging.basicConfig(level=log_level, format=fmt)
+    return logging.getLogger(file_.split("/")[-1])
