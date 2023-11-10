11db6b3483381c5b718db8abc7c32461b4a8b306
uvuik <uvuik@student.kit.edu>
2023-11-10 14:24:01 +0100

    Datei gelöscht

--- ExampleUsage.py
+++
@@ -1,7 +0,0 @@
-#Filter for Drift
-filtered_drift = EventDetection.drift_only(roorda_data, const_roorda)
-
-#Calculate and plot frequency domain
-fft, fftfreq = Filt.fft_transform(filtered_drift, const_roorda, 'x_col')
-Vis.plot_fft(fft, fftfreq)
-testchange=0
