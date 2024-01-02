0cbfb1a0e418602f57cab1b831ac97b7bd1381ff
uvuik <uvuik@student.kit.edu>
2024-01-02 15:51:17 +0100

    Augmentation finished.

--- ProcessingData/Preprocessing/Augmentation.py
+++ ProcessingData/Preprocessing/Augmentation.py
@@ -26,25 +26,6 @@ class Augmentation():
         return noisy_data
 
     @staticmethod
-    def reverse_data(dataframe, dataframe_dict):
-        """
-            Kehrt die Reihenfolge der Spalten im DataFrame außer der ersten ("Time") um.
-
-            Args:
-                dataframe (pd.DataFrame): Der DataFrame, der umgekehrt werden soll.
-
-            Returns:
-                pd.DataFrame: Der umgekehrte DataFrame mit unveränderten Zeitwerten.
-            """
-
-        reversed_data = dataframe.copy()
-        time_column_index = dataframe.columns.get_loc(dataframe_dict['time_col'])
-
-        # Wende die Umkehrung auf die ausgewählten Spalten an
-        reversed_data.iloc[:, time_column_index + 1:] = reversed_data.iloc[:, time_column_index + 1:].values[::-1]
-        return reversed_data
-
-    @staticmethod
     def slice_df(dataframe, const_dict, segment_length):
         """
            Teilt den DataFrame in Unter-DataFrames von jeweils {segment_length} Sekunden Länge auf.
@@ -52,22 +33,22 @@ class Augmentation():
            Args:
                dataframe (pd.DataFrame): Der DataFrame, der aufgeteilt werden soll.
                                          Er muss eine Spalte "Zeit" haben.
-               f (float): Die Abtastfrequenz des DataFrames.
 
            Returns:
                List[pd.DataFrame]: Eine Liste von Unter-DataFrames mit jeweils {segment_length} Sekunden Länge.
            """
         f = const_dict['f']
-        samples_per_segment = int(f*segment_length)
+        samples_per_segment = int(f * segment_length)
         segments = []
         start_idx = 0
 
-        while start_idx+samples_per_segment <= len(dataframe):
+        while start_idx + samples_per_segment <= len(dataframe):
             end_idx = start_idx + samples_per_segment
             segment = dataframe.iloc[start_idx:end_idx]
             segments.append(segment.values)
             start_idx = end_idx
         return segments
+
     @staticmethod
     def flip_dataframe(dataframe, const_dict):
         """
@@ -104,8 +85,8 @@ class Augmentation():
                              name=const_dict['time_col'])
 
         return_annot = round(pd.Series(signal.resample(interm_frame[const_dict['Annotations']], num_output_samples),
-                   name=const_dict['Annotations'])).astype(int)
-        #pd.Series(signal.resample(interm_frame[const_dict['Annotations']], num_output_samples, axis=0, method='nearest'),
+                                       name=const_dict['Annotations'])).astype(int)
+        # pd.Series(signal.resample(interm_frame[const_dict['Annotations']], num_output_samples, axis=0, method='nearest'),
         #                         name=const_dict['Annotations'])
         const_dict['f'] = f_target
         resampled_df = pd.concat([return_t, return_x, return_y, return_annot], axis=1)
